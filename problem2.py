9e62362cb55d0da73e0a56bc27fe6be5470a89cc
Joseph Derouin <joseph.derouin23@ycdsbk12.ca>
2021-02-23 15:31:11 +0000

    submit

--- problem2.py
+++ problem2.py
@@ -1,1 +1,23 @@
+'''
+-------------------------------------------------------------------------------
+Name:        problem2.py
+Purpose:    triangle checker
 
+Author:    Derouin.J
+
+Created:   23/02/2021
+------------------------------------------------------------------------------
+'''
+
+
+print("Triangle Checker")
+#Enter side lengths
+side1 = input("Enter side of length 1: ")
+side2 = input("Enter side of length 2: ")
+side3 = input("Enter side of length 3: ")
+
+
+if (side1 + side2 = side3):
+  print("Its a traingle!")
+else:
+  print("It is not a triangle.")
